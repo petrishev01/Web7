1be74be0d82fce7f9802a83fbf9561228ced1128
Alexander <poltavetz.alexander@mail.ru>
2021-10-26 17:13:01 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        infinite: false,
+        dots:true,
+        slidesToShow:4,
+        autoplay:true,
+        speed:1000,
+        autoplaySpeed:800,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow:2
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow:2
+                }
+            }
+        ]
+    });
+});
+
